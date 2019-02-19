5fb2f2f1a736b79c52c8ada2d965c4a53145ea93
Billythekidzz <gavin.ongmh@gmail.com>
2019-02-20 12:42:15 +1300

    Revert "Added ambient sounds and sound sfx for torch. Made flickering more random."
    This reverts commit dd265d971b6746adc6d7f5fc775405abb9ae02ee.

--- assignment1-game/Assets/Rusty Flashlight/Scripts/FlashlightToggle.cs
+++ assignment1-game/Assets/Rusty Flashlight/Scripts/FlashlightToggle.cs
@@ -7,126 +7,62 @@ public class FlashlightToggle : MonoBehaviour
 {
     public GameObject lightGO; //light gameObject to work with
     public bool isOn = false; //is flashlight on or off?
-    public int MAX_INSANITY = 100; //maximum insanity
-    public int currentInsanity = 0; //Current insanity
+    public int Insanity = 1; //Total Insanity meter
     public double Timer = 120; // The wait time between flashes
     public double CurrentTime = 0; //Current time for the timer;
     public bool Flicker = false;
-    public float insanityIncreaseInterval = 1.0f; //time it takes for insanity to increase in seconds (Decrease to make insanity increase faster)
-    private float time = 0.0f;
-    public float flickerChanceInterval = 1.0f; //Lights will have a chance to flicker after this many seconds (decrease for more frequent flickers)
-    private float flickerTime = 0.0f;
-    private string currentlyPlaying;
-    AudioSource[] audio;
 
     // Use this for initialization
     void Start()
     {
-        audio = GetComponents<AudioSource>();
-        //set lights on by default
-        turnLightOn();
+        //set default off
+        lightGO.SetActive(isOn);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentInsanity < MAX_INSANITY)
+        if (isOn)
         {
-            time += Time.deltaTime;
-            if (time >= insanityIncreaseInterval)
+            CurrentTime += 0.3;
+            if (Insanity > 0)
             {
-                time = 0.0f;
-                currentInsanity += 1;
-                if(currentInsanity < 20)
-                {
-                    if(currentlyPlaying != "stageone")
-                    {
-                        currentlyPlaying = "stageone";
-                        audio[5].Stop();
-                        audio[6].Play();
-                    }
-                }
-                if (currentInsanity >= 20 && currentInsanity < 40)
-                {
-                    if (currentlyPlaying != "stagetwo")
-                    {
-                        currentlyPlaying = "stagetwo";
-                        audio[6].Stop();
-                        audio[4].Stop();
-                        audio[5].Play();
-                    }
-                }
-                if (currentInsanity >= 40 && currentInsanity < 60)
-                {
-                    if (currentlyPlaying != "stagethree")
-                    {
-                        currentlyPlaying = "stagethree";
-                        audio[5].Stop();
-                        audio[3].Stop();
-                        audio[4].Play();
-                    }
-                }
-                if (currentInsanity >= 60 && currentInsanity < 80)
-                {
-                    if (currentlyPlaying != "stagefour")
-                    {
-                        currentlyPlaying = "stagefour";
-                        audio[4].Stop();
-                        audio[2].Stop();
-                        audio[3].Play();
-                    }
-                }
-                if (currentInsanity >= 80)
-                {
-                    if (currentlyPlaying != "stagefive")
-                    {
-                        currentlyPlaying = "stagefive";
-                        audio[3].Stop();
-                        audio[2].Play();
-                    }
-                }
+                Timer = 120 / Insanity;
             }
-        }
-        if (lightGO.activeSelf)
-        {
+            if (CurrentTime > 61)
+            {
+                CurrentTime = 0;
+            }
+
             if (Flicker == false)
             {
-                flickerTime += Time.deltaTime;
-                if (flickerTime >= flickerChanceInterval)
+                if (CurrentTime > Timer - 1)
                 {
-                    flickerTime = 0.0f;
-                    //currentInsanity += 1; maybe make them more insane with each flicker
-                    if (Random.Range(0.0f, 100.0f) < currentInsanity)
-                    {
-                        turnLightOff();
-                        Flicker = true;
-                        Timer = Random.Range(100.0f, 250.0f / (currentInsanity / 10));
-                        CurrentTime = 0;
-                    }
+                    //Debug.Log("DOES IT");
+                    lightGO.SetActive(false);
+                    Flicker = true;
+                    CurrentTime = 0;
                 }
+
             }
-        }
-        else
-        {
-            if (Flicker == true)
+            else
             {
-                CurrentTime += 1;
                 if (CurrentTime > Timer / 4)
                 {
                     //Debug.Log("How Quick");
-                    turnLightOn();
+                    lightGO.SetActive(true);
                     Flicker = false;
                     CurrentTime = 0;
                 }
             }
         }
-
-
-
+       
+            
+    
         //toggle flashlight on key down
-        if (Input.GetKeyDown(KeyCode.C))
+        if(Input.GetKeyDown(KeyCode.C))
         {
-            currentInsanity += 1;
+            Insanity += 1;
         }
 
         if (Input.GetKeyDown(KeyCode.X))
@@ -141,29 +77,16 @@ public class FlashlightToggle : MonoBehaviour
                     isOn = !isOn;
                     if (isOn)
                     {
-                        turnLightOn();
+                        lightGO.SetActive(true);
                     }
                     //turn light off
                     else
                     {
-                        turnLightOff();
+                        lightGO.SetActive(false);
 
                     }
                 }
         }
             
-    }
-    //Call this function to turn the light on
-    void turnLightOn()
-    {
-        lightGO.SetActive(true);
-        audio[0].Play();
-    }
-
-    //Call this function to turn the light off
-    void turnLightOff()
-    {
-        lightGO.SetActive(false);
-        audio[1].Play();
     }
 }
